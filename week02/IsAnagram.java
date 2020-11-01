1785bde0ed3e90195b2d5b0c4bd54e243a8a73d8
dadayu <714468061@qq.com>
2020-11-02 00:03:06 +0800

    新增作业

---
+++ week02/IsAnagram.java
@@ -0,0 +1,49 @@
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 有效的 字母异同位次
+ *  考点  map ~
+ */
+public class IsAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        // 效率极低
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // // 利用map +1 -1 操作
+        // Map<Character,Integer> map = new HashMap<>();
+        // for (int i = 0 ; i < s.length(); i++) {
+        //     if (s.charAt(i) ==t.charAt(i))
+        //         continue;
+        //     map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        //     map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+        // }
+        // for (Map.Entry<Character,Integer> entry : map.entrySet()){
+        //     if (entry.getValue() != 0) return false;
+        // }
+        // return true;
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] map = new int[26];
+        for (int i =0; i < s.length(); i ++) {
+            map[s.charAt(i) - 'a'] ++;
+            map[t.charAt(i) - 'a'] --;
+        }
+        // 减少了 遍历
+        for(int i =0; i < t.length(); i ++) {
+            if (map[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+}
