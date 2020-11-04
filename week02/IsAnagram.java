b398e33382de2ce4b92df9caa5ef078dcc146bfb
dadayu <714468061@qq.com>
2020-11-04 09:40:05 +0800

    新增作业

--- week02/IsAnagram.java
+++ week02/IsAnagram.java
@@ -5,11 +5,12 @@ import java.util.Map;
 /**
  * 有效的 字母异同位次
  *  考点  map ~
+ *  特点:桶思想来 来优化 map
  */
 public class IsAnagram {
 
     public boolean isAnagram(String s, String t) {
-        // 效率极低
+        // 效率较低
         // if(s.length() != t.length()){
         //     return false;
         // }
@@ -29,6 +30,7 @@ public class IsAnagram {
         if (s.length() != t.length()) {
             return false;
         }
+        // a-z 26
         int[] map = new int[26];
         for (int i =0; i < s.length(); i ++) {
             map[s.charAt(i) - 'a'] ++;
