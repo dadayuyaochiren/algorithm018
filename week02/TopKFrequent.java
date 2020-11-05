0fd0ea2cc10d79ccb86fc16c887c393dd5a2932f
dadayu <714468061@qq.com>
2020-11-05 11:07:19 +0800

    新增作业

--- week02/TopKFrequent.java
+++ week02/TopKFrequent.java
@@ -18,7 +18,7 @@ public class TopKFrequent {
         PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(new Comparator<Map.Entry<Integer, Integer>>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o2.getValue() - o1.getValue();  // 默认降序
+                return o2.getValue() - o1.getValue();  // 降序
             }
         });
         //全部放入
